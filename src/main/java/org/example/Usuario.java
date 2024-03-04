7be3322c1ef00083b289f1d2a9a1fc1811e46503
Meruzzi <gabrielmeruzzi@gmail.com>
2024-03-04 16:59:28 -0300

    Start

--- src/main/java/org/example/Usuario.java
+++ src/main/java/org/example/Usuario.java
@@ -4,9 +4,7 @@ import lombok.Getter;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 
 
 @Setter
@@ -16,108 +14,63 @@ public class Usuario {
     private int Pontos;
     private List<Tarefa> ListaTarefas;
 
-
     public Usuario(String nome) {
         this.Nome = nome;
         this.Pontos = 0;
         this.ListaTarefas = new ArrayList<>();
     }
 
-    Scanner input = new Scanner(System.in);
-
-    public void VisualizarListaTarefas() {
-        for(var tarefa : ListaTarefas) {
-            System.out.println("Tarefa: " + tarefa.getTitulo());
-            System.out.println("Situação: " + (tarefa.isSituacao() ? "Concluída" : "Não Concluída" ));
+    public boolean AdicionarTarefa(Tarefa tarefa) {
+        if(tarefa != null) {
+            ListaTarefas.add(tarefa);
+            return true;
+        } else {
+            return false;
         }
     }
 
-    public void AdicionarTarefa(Tarefa tarefa) {
-        ListaTarefas.add(tarefa);
-    }
-
-    public Tarefa VisualizarDetalhesTarefa() {
-        System.out.println("Tarefas cadastradas:");
-        for (var tarefa : ListaTarefas) {
-            System.out.println("Tarefa: " + tarefa.getTitulo());
-            System.out.println("Situação: " + (tarefa.isSituacao() ? "Concluída" : "Não Concluída"));
-        }
-
-        System.out.println("Insira o nome da tarefa para ver detalhes:");
-        String nomeTarefa = input.nextLine();
-
+    public Tarefa VisualizarDetalhesTarefa(Tarefa tarefa) {
         for (var i = 0; i < ListaTarefas.size(); i++) {
-            if (ListaTarefas.get(i).getTitulo().equals(nomeTarefa)) {
-                Tarefa TarefaEscolhida = ListaTarefas.get(i);
-                System.out.println("Tarefa: " + TarefaEscolhida.getTitulo());
-                System.out.println("Descrição: " + TarefaEscolhida.getDescricao());
-                System.out.println("Data de criação: " + TarefaEscolhida.getDataCriacao());
-                System.out.println("Data de conclusão: " + TarefaEscolhida.getDataConclusao());
-                System.out.println("Situação: " + (TarefaEscolhida.isSituacao() ? "Concluída" : "Não Concluída"));
-                return TarefaEscolhida;
-            } else {
-                System.out.println("Tarefa não encontrada.");
+            if (ListaTarefas.get(i).equals(tarefa)) {
+                return ListaTarefas.get(i);
             }
         }
         return null;
     }
 
-    public void TarefaConcluida() {
-        VisualizarListaTarefas();
-
-        System.out.println("Insira o nome da tarefa que deseja marcar como concluída:");
-        String nomeTarefa = input.nextLine();
-
-        boolean tarefaEncontrada = false;
-
+    public boolean TarefaConcluida(Tarefa tarefa, LocalDate DataConclusao) {
         for (var i = 0; i < ListaTarefas.size(); i++) {
-            if (ListaTarefas.get(i).getTitulo().equals(nomeTarefa)) {
+            if (ListaTarefas.get(i).equals(tarefa)) {
                 Tarefa TarefaEscolhida = ListaTarefas.get(i);
                 TarefaEscolhida.setSituacao(true);
-
-                System.out.println("Insira a data de conclusão (yyyy-MM-dd):");
-                String dataConclusaoTarefaStr = input.nextLine();
-                LocalDate dataConclusao = LocalDate.parse(dataConclusaoTarefaStr, DateTimeFormatter.ISO_DATE);
-                TarefaEscolhida.setDataConclusao(dataConclusao);
-
-
-                System.out.println("Tarefa " + TarefaEscolhida.getTitulo() + " foi concluída.");
-
-                verificarPontuacao(TarefaEscolhida);
-                tarefaEncontrada = true;
-                break;
+                TarefaEscolhida.setDataConclusao(DataConclusao);
+                verificarPontuacao(DataConclusao);
+                return true;
             }
         }
-        if (!tarefaEncontrada) {
-            System.out.println("Tarefa não encontrada.");
-        }
+        return false;
     }
 
 
-    public void verificarPontuacao(Tarefa tarefa) {
+    public int verificarPontuacao(LocalDate DataConclusao) {
         int contadorTarefasConcluidasDia = 0;
-        LocalDate dataTarefa = tarefa.getDataConclusao();
 
         for (Tarefa t : ListaTarefas) {
-            if (t.isSituacao() && t.getDataConclusao() != null && t.getDataConclusao().isEqual(dataTarefa)) {
+            if (t.isSituacao() && t.getDataConclusao() != null && t.getDataConclusao().isEqual(DataConclusao)) {
                 contadorTarefasConcluidasDia++;
             }
         }
 
-        if (contadorTarefasConcluidasDia == 5) {
-            Pontos += 3;
-            System.out.println("Tarefas concluídas no dia: " + contadorTarefasConcluidasDia + ", pontuação: " + this.Pontos);
-        } else if (contadorTarefasConcluidasDia == 3) {
-            Pontos += 2;
-            System.out.println("Tarefas concluídas no dia: " + contadorTarefasConcluidasDia + ", pontuação: " + this.Pontos);
-        } else {
-            Pontos += 1;
-            System.out.println("Tarefas concluídas no dia: " + contadorTarefasConcluidasDia + ", pontuação: " + this.Pontos);
+        if (contadorTarefasConcluidasDia == 0) {
+            return Pontos -= 1;
         }
 
-        if (contadorTarefasConcluidasDia == 0) {
-            Pontos -= 1;
-            System.out.println("Nenhuma tarefa concluída hoje. Pontos: " + this.Pontos);
+        if (contadorTarefasConcluidasDia == 5) {
+            return Pontos += 3;
+        } else if (contadorTarefasConcluidasDia == 3) {
+            return Pontos += 2;
+        } else {
+            return Pontos += 1;
         }
     }
 }
--- src/test/java/TarefasTest.java
+++ src/test/java/TarefasTest.java
@@ -5,6 +5,7 @@
     import org.junit.jupiter.api.Test;
 
     import java.util.List;
+    import java.time.LocalDate;
 
     import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -37,17 +38,106 @@
             usuario.AdicionarTarefa(tarefa1);
             usuario.AdicionarTarefa(tarefa2);
 
-            usuario.VisualizarDetalhesTarefa();
+            Tarefa actual = usuario.VisualizarDetalhesTarefa(tarefa2);
 
-
+            Assertions.assertEquals(tarefa2, actual);
 
         }
 
-        @Test @DisplayName("Deve retornar")
+        @Test @DisplayName("Teste adicionar tarefa")
         void TesteAdicionarTarefa() {
             var User1 = new Usuario("Gabriel");
             var Tarefa1 = new Tarefa("1", "Testando1");
 
-            User1.AdicionarTarefa(Tarefa1);
+            Assertions.assertTrue(User1.AdicionarTarefa(Tarefa1));
         }
+
+        @Test @DisplayName("Teste Tarefa Concluida")
+        void TesteTarefaConcluida() {
+            var User1 = new Usuario("Gabriel");
+            var Tarefa1 = new Tarefa("1", "Testando1");
+
+            User1.AdicionarTarefa(Tarefa1);
+            LocalDate dataEspecifica = LocalDate.of(2024, 3, 4);
+
+            Tarefa tr1 = User1.VisualizarDetalhesTarefa(Tarefa1);
+
+            Assertions.assertTrue(User1.TarefaConcluida(Tarefa1, dataEspecifica));
+            Assertions.assertTrue(tr1.isSituacao());
+        }
+
+        @Test @DisplayName("Teste pontuação +1 tarefa concluida no dia")
+        void TestePontuacao1() {
+            var User1 = new Usuario("Gabriel");
+            var Tarefa1 = new Tarefa("1", "Testando1");
+
+            User1.AdicionarTarefa(Tarefa1);
+            LocalDate dataEspecifica = LocalDate.of(2024, 3, 4);
+
+            User1.TarefaConcluida(Tarefa1, dataEspecifica);
+
+            int pontos = User1.getPontos();
+
+            Assertions.assertEquals(1, pontos);
+        }
+        @Test @DisplayName("Teste pontuação +3 tarefa concluida no dia")
+        void TestePontuacao3() {
+            var User1 = new Usuario("Gabriel");
+            var Tarefa1 = new Tarefa("1", "Testando1");
+            var Tarefa2 = new Tarefa("2", "Testando2");
+            var Tarefa3 = new Tarefa("3", "Testando3");
+
+            User1.AdicionarTarefa(Tarefa1);
+            User1.AdicionarTarefa(Tarefa2);
+            User1.AdicionarTarefa(Tarefa3);
+            LocalDate dataEspecifica = LocalDate.of(2024, 3, 5);
+
+            User1.TarefaConcluida(Tarefa1, dataEspecifica);
+            User1.TarefaConcluida(Tarefa2, dataEspecifica);
+            User1.TarefaConcluida(Tarefa3, dataEspecifica);
+
+            int pontos = User1.getPontos();
+
+            Assertions.assertEquals(4, pontos);
+        }
+        @Test @DisplayName("Teste pontuação +5 tarefa concluida no dia")
+        void TestePontuacao5() {
+            var User1 = new Usuario("Gabriel");
+            var Tarefa1 = new Tarefa("1", "Testando1");
+            var Tarefa2 = new Tarefa("2", "Testando2");
+            var Tarefa3 = new Tarefa("3", "Testando3");
+            var Tarefa4 = new Tarefa("4", "Testando4");
+            var Tarefa5 = new Tarefa("5", "Testando5");
+
+            User1.AdicionarTarefa(Tarefa1);
+            User1.AdicionarTarefa(Tarefa2);
+            User1.AdicionarTarefa(Tarefa3);
+            User1.AdicionarTarefa(Tarefa4);
+            User1.AdicionarTarefa(Tarefa5);
+            LocalDate dataEspecifica = LocalDate.of(2024, 3, 6);
+
+            User1.TarefaConcluida(Tarefa1, dataEspecifica);
+            User1.TarefaConcluida(Tarefa2, dataEspecifica);
+            User1.TarefaConcluida(Tarefa3, dataEspecifica);
+            User1.TarefaConcluida(Tarefa4, dataEspecifica);
+            User1.TarefaConcluida(Tarefa5, dataEspecifica);
+
+            int pontos = User1.getPontos();
+
+            Assertions.assertEquals(8, pontos);
+        }
+
+        @Test @DisplayName("Teste pontuação -1 tarefa concluida no dia")
+        void TestePontuacaoMenos1() {
+            var User1 = new Usuario("Gabriel");
+
+            LocalDate dataEspecifica = LocalDate.of(2024, 3, 4);
+
+            User1.verificarPontuacao(dataEspecifica);
+
+            int pontos = User1.getPontos();
+
+            Assertions.assertEquals(-1, pontos);
+        }
+
     }
